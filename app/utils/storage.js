ac500a045152661ce7b6ff565a8542c739691b61
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-12-23 09:01:22 +0000

    Add missing util functions

--- app/utils/storage.js
+++ app/utils/storage.js
@@ -1,3 +1,6 @@
+import { StorageSharedKeyCredential } from '@azure/storage-blob'
+import { DefaultAzureCredential } from '@azure/identity'
+
 import { storage } from '../config/index.js'
 
 const getStorageEndpoint = (endpoint, accountName) => {
@@ -6,11 +9,15 @@ const getStorageEndpoint = (endpoint, accountName) => {
 
 const getStorageCredential = (accountName, accessKey) => {
   if (accessKey) {
+    console.log('Using Azure Storage Shared Key Credential for account:', accountName)
+
     return new StorageSharedKeyCredential(
       accountName,
       accessKey
     )
   }
+
+  console.log('Using Azure Identity Credential for account:', accountName)
 
   return new DefaultAzureCredential({ managedIdentityClientId: storage.get('managedIdentityClientId') })
 }

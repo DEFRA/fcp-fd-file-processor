4c9cb5aa777fb3f44c09943996b2fd5c2ce7c4c7
jungabunga <amir.ishmuhametov@gmail.com>
2025-01-10 09:48:53 +0000

    removed unused code

--- app/utils/storage.js
+++ app/utils/storage.js
@@ -1,18 +1,6 @@
 import { StorageSharedKeyCredential } from '@azure/storage-blob'
-import isProd from '../utils/is-prod.js'
 import { storage } from '../config/index.js'
-
-import { DefaultAzureCredential, getBearerTokenProvider } from '@azure/identity'
-
-if (isProd()) {
-  storage.hooks = {
-    beforeConnect: async (cfg) => {
-      const credential = new DefaultAzureCredential({ managedIdentityClientId: process.env.AZURE_CLIENT_ID })
-      const tokenProvider = getBearerTokenProvider(credential, 'https://ossrdbms-aad.database.windows.net/.default')
-      cfg.password = tokenProvider
-    }
-  }
-}
+import { DefaultAzureCredential } from '@azure/identity'
 
 const getStorageEndpoint = (endpoint, accountName) => {
   return endpoint.replace('?', accountName)
